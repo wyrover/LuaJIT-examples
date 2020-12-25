9460a291b8132eb13c624dfa7e63977cb652a00e
wyrover <wyrover@gmail.com>
2020-12-25 14:52:59 +0800

    * CMakeLists.txt

---
+++ src/examples/LuaJIT-examples.cpp
@@ -0,0 +1,29 @@
+﻿#if SNIPPET01
+
+#include <LuaJIT\src\lua.hpp>
+#include <iostream>
+
+
+
+#pragma comment(lib,"lua51.lib")
+extern "C"
+{
+#include <LuaJIT\src\lua.h>
+#include <LuaJIT\src\lualib.h> 
+}
+
+int main(int argc, char* argv[])
+{
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+
+    luaL_loadfile(L, "script.lua");
+    lua_pcall(L, 0, 0, 0);
+    lua_close(L);
+    
+    system("pause");
+    return 0;
+}
+
+
+#endif
